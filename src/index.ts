c759695163e8cc32ce07a324695016bd4ea2c653
Jorge989 <attiejorge89@gmail.com>
2021-06-12 20:24:18 -0300

    primeiro

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+const cors = require("cors");
 import routes from "./routes";
-import "./database/connect";
+import "../src/database/connect";
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 app.listen(3000, () =>
   console.log("🔥 Server strated at http://localhost:3000")
